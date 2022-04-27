4a7c2d229665940901f784a758fd22e924a729b0
magicHack3r <dnjsdlf61@naver.com>
2022-04-27 11:29:47 -0400

    query with nip add

--- gather.py
+++ gather.py
@@ -1,40 +1,62 @@
 import shodan,os 
-
+from censys.search import CensysHosts
 
 class Init():
 
     def __init__(self):
-        self.shodan_id = None
-        self.shodan_secret = None
-        self.censys_id = None
-        self.censys_secret = None
+        self.shodan_api = None
 
-    def config(self,filename):
-        self.set_credentials(filename)
+    def config(self,shodan_api_filename):
+        self.set_credentials(shodan_api_filename)
        
-    def write_credentials(self, filename):
-        with open(filename, 'wt') as fp:
-            fp.write(self.shodan_id+'\n')
-            fp.write(self.shodan_secret+'\n')
-            fp.write(self.censys_id+'\n')
-            fp.write(self.censys_secret+'\n')
-            
-    def set_credentials(self, filename):
-        if not os.path.exists(filename):
+    def set_credentials(self, shodan_api_filename):
+        if not os.path.exists(shodan_api_filename):
             print('[!!] credentials was not setup!')
-            self.shodan_id = input('[!] input shodan_id: ')
-            self.shodan_secret  = input('[!] input shodan_secret: ') 
-            self.censys_id = input('[!] input censys_id: ')
-            self.censys_secret = input('[!] input censys_secret: ')
-            self.write_credentials(filename)
+            self.shodan_api = input('[!] input shodan_api: ')
+            with open(shodan_api_filename, 'wt') as fp:
+                fp.write(self.shodan_api)
 
-        else:
-            with open(filename, 'rt') as fp:
-                self.shodan_id = fp.readline()
-                self.shodan_secret = fp.readline()
-                self.censys_id = fp.readline()
-                self.censys_secret = fp.readline()
+        if not os.path.exists('~/.config/censys/censys.cfg'):
+            os.system('censys config')
 
+        with open(shodan_api_filename, 'rt') as fp:
+            self.shodan_api = fp.readline()
+
+    def get_shodan_api(self):
+        return self.shodan_api
+
+
+def query_shodan_with_nip(api_key,  nip):
+
+    print()
+    print('[+] resluts of censys query')
+    try:
+        api = shodan.Shodan(api_key)
+        query = f'net:{nip}'
+        result = api.search(query)
+
+        for service in result['matches']:
+            print(service['ip_str'])
+
+    except Exception as e:
+        print(e)
+
+h = CensysHosts()
+def query_censys_with_nip(nip):
+
+    print()
+    print('[+] resluts of censys query')
+    print('[INFO] ip => [ service/port ... coutnry_code]')
+    query = h.search(f'ip:{nip}', per_page=5, page=2)
+    for page in query:
+        for host in page:
+            print(host['ip'], end='' )
+
+            print(' => [ ', end = '')
+            for service in host['services']:
+                print(f'{service["service_name"]}/{service["port"]} ', end='')
+
+            print(host['location']['country_code']+']')
 
 
 def print_banner():
@@ -75,10 +97,8 @@ if __name__ == '__main__':
             pass
 
         elif cmd == 'nscan':
-            nip = input('[*] input network ip address: ')
-            pass
-
-
-
+            nip = input('[*] input network ip address ex) 23.0.0.0/8: ')
+            query_shodan_with_nip(init.get_shodan_api(), nip)
+            query_censys_with_nip(nip)
 
 
